152075ff283aeb4141fa56b3aec9d12b7ffa6b5a
lina <jack@jackli.dev>
2022-07-25 17:52:04 -0700

    feat: add documention to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 export const Header = () => {
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
     return (
         <>
             <div className="sticky top-0 bg-navbar-bg z-50 navbar">
                 <div className="font-kgcs flex items-center px-8 py-5 mx-auto max-w-[1300px] text-2xl">
                     <h1>sugoiart</h1>
-                    <ul className="flex gap-[20px] list-none text-white/70 ml-[40px] text-xl flex-1">
+                    <ul className="flex gap-[20px] list-none text-white/70 ml-[40px] text-xl flex-1 font-lexbold">
                         <li>
-                            <a href="#" className="hover:text-white/90 transition-all duration-200 ease-in-out">home</a>
+                            <a href="#" onClick={scrollToTop} className="hover:text-white/90 transition-all duration-200 ease-in-out">home</a>
+                        </li>
+                        <li>
+                            <a href="#documentation" className="hover:text-white/90 transition-all duration-200 ease-in-out">documentation</a>
                         </li>
                     </ul>
                     <div className="flex">
